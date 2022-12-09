dfcee0e15283b2f4f23484a020ac5bc703446d89
Fernando <fernandostarlin1012@gmail.com>
2022-12-09 12:09:37 -0400

    Starting the path

---
+++ 01- Fundamentals/assets/JS/alerts.js
@@ -0,0 +1,8 @@
+alert("Hola Mundo");
+let name = prompt("Como te llamas?", "without name");
+console.log(name);
+
+const confirmation = confirm("Are u sure?");
+console.log(confirmation);
+
+// console.log(global);
---
+++ 01- Fundamentals/assets/JS/app.js
@@ -0,0 +1,24 @@
+console.log('Hola Mundo');
+
+let a = 10;
+let b = 10;
+let x = a + b;
+console.log(x);
+
+let c = 10,
+    d = 20,
+    y = c + d,
+    greeting = "Hola ",
+    greeting2 = "IronMan";
+
+const www = greeting + greeting2;
+
+console.log(y);
+console.log({ c })
+console.log({ d })
+console.log({ y })
+console.warn(c)
+console.error(d)
+console.table([c, d, y])
+console.log('%c Esto no es un title', 'color:blue; font-weight:bold;')
+console.log(www)
