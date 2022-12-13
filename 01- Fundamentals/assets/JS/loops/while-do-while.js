7229bae540e736a8b8161afaf48b35864aba1c4a
Fernando <fernandostarlin1012@gmail.com>
2022-12-12 21:50:48 -0400

    Working with while and do while loops

---
+++ 01- Fundamentals/assets/JS/loops/while-do-while.js
@@ -0,0 +1,15 @@
+const cars = ['Ford', 'Mazda', 'Honda', 'Toyota']
+
+let i = 0;
+/*while (i < cars.length) {
+    console.log(cars[i]);
+    i++; // i = i + 1, i += 1;
+
+}*/
+
+// Do While
+
+do {
+    console.log(cars[i]);
+    i++;
+} while (cars[i]);
