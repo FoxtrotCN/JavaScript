1d1520c2d6fb9416629c5069b87665c80c4e7f7e
Fernando <fernandostarlin1012@gmail.com>
2022-12-19 15:05:48 -0400

    Using methods into our classes

--- 01- Fundamentals/assets/JS/classes/class.js
+++ 01- Fundamentals/assets/JS/classes/class.js
@@ -2,10 +2,20 @@ class Person {
     name;
     code;
     phrase;
+
     constructor(name, code, phrase) {
         this.name = name;
         this.code = code;
         this.phrase = phrase;
+    }
+
+    whoAmI() {
+        console.log(`I am ${this.name} better known as ${this.code}`);
+    }
+
+    myPhrase() {
+        this.whoAmI();
+        console.log(`${this.code} says: ${this.phrase}`);
     }
 }
 
@@ -14,3 +24,6 @@ const spiderMan = new Person('Peter Parker', 'SpiderMan', 'I am spider man' );
 
 console.log(ironMan);
 console.log(spiderMan);
+
+ironMan.myPhrase();
+spiderMan.myPhrase();
