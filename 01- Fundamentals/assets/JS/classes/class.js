39a549e44335daf37acd2f4bd3c1bec084d0ed39
Fernando <fernandostarlin1012@gmail.com>
2022-12-19 15:18:36 -0400

    Using sets and gets

--- 01- Fundamentals/assets/JS/classes/class.js
+++ 01- Fundamentals/assets/JS/classes/class.js
@@ -1,12 +1,21 @@
 class Person {
-    name;
-    code;
-    phrase;
+    name = '';
+    code = '';
+    phrase = '';
+    food = '';
 
     constructor(name, code, phrase) {
         this.name = name;
         this.code = code;
         this.phrase = phrase;
+    }
+
+    set setFavoriteFood(food) {
+        this.food = food.toUpperCase();
+    }
+
+    get getFavoriteFood() {
+        return `The ${this.name} favorite food is ${this.food}`;
     }
 
     whoAmI() {
@@ -19,11 +28,14 @@ class Person {
     }
 }
 
-const ironMan = new Person('Tony Stark', 'IronMan', 'I am iron man' );
-const spiderMan = new Person('Peter Parker', 'SpiderMan', 'I am spider man' );
+const ironMan = new Person('Tony Stark', 'IronMan', 'I am iron man');
+const spiderMan = new Person('Peter Parker', 'SpiderMan', 'I am spider man');
 
 console.log(ironMan);
 console.log(spiderMan);
 
 ironMan.myPhrase();
-spiderMan.myPhrase();
+// spiderMan.myPhrase();
+ironMan.setFavoriteFood = `Carne de Re'`;
+console.log(ironMan);
+console.log(ironMan.getFavoriteFood);
