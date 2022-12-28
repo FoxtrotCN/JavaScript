02677f53fdaba3f75cd8428f8cbc0e99c0f322e7
Fernando <fernandostarlin1012@gmail.com>
2022-12-28 16:44:58 -0400

    In this section We've installed the Html loader and Html webpack plugin as a part of our app configuration. We also set some rules in the webpack.config.js for a better performance. Stay tuned!

--- 03- webpack-initial/webpack.config.js
+++ 03- webpack-initial/webpack.config.js
@@ -1,13 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
 
+    output: {
+        clean: true
+    },
+
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                options: {
+                    sources: false
+                }
+            }
+        ]
     },
 
     optimization: {
 
     },
 
-    plugins: []
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
 }
