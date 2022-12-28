6426c0fda5b6f4dc41e6a97c1a45eee6fa2f6f92
Fernando <fernandostarlin1012@gmail.com>
2022-12-28 18:18:45 -0400

    Working with IMG files in Webpack

--- 03- webpack-initial/src/js/components.js
+++ 03- webpack-initial/src/js/components.js
@@ -1,4 +1,5 @@
 import '../css/components.css';
+import webpackLogo from '../assets/img/webpack-logo.png';
 
 export const greeting = (name) => {
     console.log('Making an h1 tag');
@@ -6,4 +7,9 @@ export const greeting = (name) => {
     const h1 = document.createElement('h1');
     h1.innerText = `Hi, ${name}`;
     document.body.append(h1);
+
+    //IMG
+    const img = document.createElement('img');
+    img.src = webpackLogo;
+    document.body.append(img);
 }
--- 03- webpack-initial/webpack.config.js
+++ 03- webpack-initial/webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
             {
                 test: /styles.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: "file-loader"
             }
         ]
     },
