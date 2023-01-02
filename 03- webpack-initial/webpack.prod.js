cf8c135e2618191fa5ba8b092592e830063113fc
Fernando <fernandostarlin1012@gmail.com>
2023-01-02 13:11:47 -0400

    Minimizing the main CSS file for production mode

--- 03- webpack-initial/webpack.prod.js
+++ 03- webpack-initial/webpack.prod.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: 'production',
@@ -37,6 +39,11 @@ module.exports = {
     },
 
     optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
 
     },
 
